18d6315a121be0d184b9ca02271899481f97e9de
Aswin Suresh <aswin3577@gmail.com>
2026-05-23 21:12:24 +0530

    updated

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ app.use("/resumeUploads", express.static("resumeUploads"));
 //socket.io server creation
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://nexthire-mainproject-frontend-kps5.vercel.app"
+    ],
     methods: ["GET", "POST"],
   },
 });
